6a5aa86a3d8d88890185828038feb93641b4287a
Kristoffer Andersen <ka@siteshop.dk>
2021-12-02 13:42:18 +0100

    2020 Day 2

---
+++ AdventOfCode2020/Day2/Day2.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using Utilities;
+
+namespace AdventOfCode2020.Day2
+{
+    public static class Day2
+    {
+        private static string day = MethodBase.GetCurrentMethod().DeclaringType.Name;
+
+        public static void CalculateA()
+        {
+            var passwords = IO.ReadInputFileStringArray(day, "a");
+            int count = 0;
+            Password pass = new();
+            foreach (var password in passwords)
+            {
+                pass.ParsePassword(password);
+                if (pass.ValidateA())
+                    count++;
+            }
+
+            IO.WriteOutput(day, "a", count.ToString());
+        }
+        public static void CalculateB()
+        {
+            var passwords = IO.ReadInputFileStringArray(day, "a");
+            int count = 0;
+            Password pass = new();
+            foreach (var password in passwords)
+            {
+                pass.ParsePassword(password);
+                if (pass.ValidateB())
+                    count++;
+            }
+
+            IO.WriteOutput(day, "b", count.ToString());
+        }
+    }
+}
---
+++ AdventOfCode2020/Day2/Password.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Day2
+{
+    class Password
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public char character { get; set; }
+        public string password { get; set; }
+
+        public void ParsePassword(string raw)
+        {
+            var splitList = raw.Split(' ').ToArray();
+            var minMax = splitList[0].Split('-');
+            Min = int.Parse(minMax[0]);
+            Max = int.Parse(minMax[1]);
+
+            character = char.Parse(splitList[1].Substring(0, 1));
+
+            password = splitList[2];
+        }
+
+        public bool ValidateA()
+        {
+            int count = password.Count(x => x == character);
+            return count <= Max && count >= Min;
+        }
+
+        public bool ValidateB()
+        {
+            bool exists = password[Min - 1] == character || password[Max - 1] == character;
+            bool both = password[Min - 1] == character && password[Max - 1] == character;
+            return exists && !both;
+        }
+    }
+}
