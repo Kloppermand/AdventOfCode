14d06e61bf90cd10427f43faf971c832ece39498
Kristoffer (Klopper) Andersen <ka@siteshop.dk>
2022-12-02 10:46:12 +0100

    2022 day2

---
+++ AdventOfCode2022/Day2/Day2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Utilities;
+
+namespace AdventOfCode2022.Day2
+{
+    public static class Day2
+    {
+        private static string day = MethodBase.GetCurrentMethod().DeclaringType.Name;
+
+        public static void CalculateA()
+        {
+            var input = IO.ReadInputFileStringArray(day, "a");
+            Dictionary<char, int> shapeScore = new() { { 'X', 1 }, { 'Y', 2 }, { 'Z', 3 }, { 'A', 1 }, { 'B', 2 }, { 'C', 3 } };
+
+            int score = input.Sum(round => shapeScore[round[2]] + ((shapeScore[round[2]] - shapeScore[round[0]] + 4) % 3) * 3);
+
+            IO.WriteOutput(day, "a", score);
+        }
+        public static void CalculateB()
+        {
+            var input = IO.ReadInputFileStringArray(day, "a");
+            Dictionary<char, int> shapeScore = new() { { 'X', -1 }, { 'Y', 0 }, { 'Z', 1 }, { 'A', 1 }, { 'B', 2 }, { 'C', 3 } };
+
+            var score = input.Sum(round => (shapeScore[round[0]] + shapeScore[round[2]] + 2) % 3 + 1 + (shapeScore[round[2]] + 1) * 3);
+
+            IO.WriteOutput(day, "b", score);
+        }
+    }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -6,8 +6,11 @@ namespace AdventOfCode2022
     {
         static void Main(string[] args)
         {
-            Day1.Day1.CalculateA();
-            Day1.Day1.CalculateB();
+            //Day1.Day1.CalculateA();
+            //Day1.Day1.CalculateB();
+
+            Day2.Day2.CalculateA();
+            Day2.Day2.CalculateB();
         }
     }
 }
