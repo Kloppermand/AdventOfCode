ca60c3b5c045c9834e299fbcb5cd60c3357e8ac9
Kristoffer (Klopper) Andersen <ka@siteshop.dk>
2022-12-02 11:18:46 +0100

    2022 Day2, removed Dictionary to one Linq, using char values.

--- AdventOfCode2022/Day2/Day2.cs
+++ AdventOfCode2022/Day2/Day2.cs
@@ -13,18 +13,16 @@ namespace AdventOfCode2022.Day2
         public static void CalculateA()
         {
             var input = IO.ReadInputFileStringArray(day, "a");
-            Dictionary<char, int> shapeScore = new() { { 'X', 1 }, { 'Y', 2 }, { 'Z', 3 }, { 'A', 1 }, { 'B', 2 }, { 'C', 3 } };
 
-            int score = input.Sum(round => shapeScore[round[2]] + ((shapeScore[round[2]] - shapeScore[round[0]] + 4) % 3) * 3);
+            int score = input.Sum(round => round[2]-87 + ((round[2] - round[0] -19) % 3) * 3);
 
             IO.WriteOutput(day, "a", score);
         }
         public static void CalculateB()
         {
             var input = IO.ReadInputFileStringArray(day, "a");
-            Dictionary<char, int> shapeScore = new() { { 'X', -1 }, { 'Y', 0 }, { 'Z', 1 }, { 'A', 1 }, { 'B', 2 }, { 'C', 3 } };
 
-            var score = input.Sum(round => (shapeScore[round[0]] + shapeScore[round[2]] + 2) % 3 + 1 + (shapeScore[round[2]] + 1) * 3);
+            var score = input.Sum(round => (round[0] + round[2]-151) % 3 + 1 + (round[2]-88) * 3);
 
             IO.WriteOutput(day, "b", score);
         }
