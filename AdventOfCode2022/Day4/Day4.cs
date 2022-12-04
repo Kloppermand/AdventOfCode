e5b2333bfa6df1b8a66301d83d5b00592a8a950b
kloppermand <kloppermand@hotmail.com>
2022-12-04 12:23:32 +0100

    2022 Day4 Use Select for better Linq variables.

--- AdventOfCode2022/Day4/Day4.cs
+++ AdventOfCode2022/Day4/Day4.cs
@@ -20,25 +20,35 @@ namespace AdventOfCode2022.Day4
                 int.Parse(x.Split(',')[0].Split('-')[1]) <= int.Parse(x.Split(',')[1].Split('-')[1]))
                 ).Count();
 
+            int fap = input.Select(x => new
+            {
+                S1 = int.Parse(x.Split(',')[0].Split('-')[0]),
+                E1 = int.Parse(x.Split(',')[0].Split('-')[1]),
+                S2 = int.Parse(x.Split(',')[1].Split('-')[0]),
+                E2 = int.Parse(x.Split(',')[1].Split('-')[1])
+            }).Where(y =>
+            (y.S1 <= y.S2 && y.E1 >= y.E2) ||
+            (y.S1 >= y.S2 && y.E1 <= y.E2)
+            ).Count();
+
             IO.WriteOutput(day, "a", result);
         }
         public static void CalculateB()
         {
             var input = IO.ReadInputFileStringArray(day, "a");
 
-            int result = input.Where(x =>
-                (int.Parse(x.Split(',')[0].Split('-')[0]) >= int.Parse(x.Split(',')[1].Split('-')[0]) &&
-                int.Parse(x.Split(',')[0].Split('-')[0]) <= int.Parse(x.Split(',')[1].Split('-')[1])) ||
-
-                (int.Parse(x.Split(',')[0].Split('-')[1]) >= int.Parse(x.Split(',')[1].Split('-')[0]) &&
-                int.Parse(x.Split(',')[0].Split('-')[1]) <= int.Parse(x.Split(',')[1].Split('-')[1])) ||
-
-                (int.Parse(x.Split(',')[1].Split('-')[0]) >= int.Parse(x.Split(',')[0].Split('-')[0]) &&
-                int.Parse(x.Split(',')[1].Split('-')[0]) <= int.Parse(x.Split(',')[0].Split('-')[1])) ||
-
-                (int.Parse(x.Split(',')[1].Split('-')[1]) >= int.Parse(x.Split(',')[0].Split('-')[0]) &&
-                int.Parse(x.Split(',')[1].Split('-')[1]) <= int.Parse(x.Split(',')[0].Split('-')[0])) 
-                ).Count();
+            int result = input.Select(x => new
+            {
+                S1 = int.Parse(x.Split(',')[0].Split('-')[0]),
+                E1 = int.Parse(x.Split(',')[0].Split('-')[1]),
+                S2 = int.Parse(x.Split(',')[1].Split('-')[0]),
+                E2 = int.Parse(x.Split(',')[1].Split('-')[1])
+            }).Where(y => 
+            (y.S1 >= y.S2 && y.S1 <= y.E2) ||
+            (y.E1 >= y.S2 && y.E1 <= y.E2) ||
+            (y.S2 >= y.S1 && y.S2 <= y.E1) ||
+            (y.E2 >= y.S1 && y.E2 <= y.E1)
+            ).Count();
 
             IO.WriteOutput(day, "b", result);
         }
