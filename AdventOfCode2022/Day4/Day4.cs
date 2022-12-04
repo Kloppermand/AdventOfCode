9441429533290f9c1dfdf5d9e8e0f3f4b41422c1
kloppermand <kloppermand@hotmail.com>
2022-12-04 10:36:46 +0100

    2022 Day4

---
+++ AdventOfCode2022/Day4/Day4.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Utilities;
+
+namespace AdventOfCode2022.Day4
+{
+    public static class Day4
+    {
+        private static string day = MethodBase.GetCurrentMethod().DeclaringType.Name;
+
+        public static void CalculateA()
+        {
+            var input = IO.ReadInputFileStringArray(day, "a");
+            int result = 0;
+
+            foreach (var pair in input)
+            {
+                var sections = pair.Split(',');
+                var elf1 = sections[0].Split('-').Select(x => int.Parse(x)).ToArray();
+                var elf2 = sections[1].Split('-').Select(x => int.Parse(x)).ToArray();
+
+                if (elf1[0] <= elf2[0] && elf1[1] >= elf2[1])
+                    result++;
+                else if (elf1[0] >= elf2[0] && elf1[1] <= elf2[1])
+                    result++;
+            }
+
+            IO.WriteOutput(day, "a", result);
+        }
+        public static void CalculateB()
+        {
+            var input = IO.ReadInputFileStringArray(day, "a");
+            int result = 0;
+
+            foreach (var pair in input)
+            {
+                var sections = pair.Split(',');
+                var elf1 = sections[0].Split('-').Select(x => int.Parse(x)).ToArray();
+                var elf2 = sections[1].Split('-').Select(x => int.Parse(x)).ToArray();
+
+                if (elf1[0] >= elf2[0] && elf1[0] <= elf2[1])
+                    result++;
+                else if (elf1[1] >= elf2[0] && elf1[1] <= elf2[1])
+                    result++;
+                else if (elf2[0] >= elf1[0] && elf2[0] <= elf1[1])
+                    result++;
+                else if (elf2[1] >= elf1[0] && elf2[1] <= elf1[1])
+                    result++;
+            }
+
+            IO.WriteOutput(day, "b", result);
+        }
+    }
+}
