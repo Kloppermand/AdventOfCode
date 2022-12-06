150b0d52aa18d1e8c4696ce6a2f27e70e1b695b1
Kristoffer (Klopper) Andersen <ka@siteshop.dk>
2022-12-06 09:35:37 +0100

    2022 Day5

---
+++ AdventOfCode2022/Day6/Day6.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Utilities;
+
+namespace AdventOfCode2022.Day6
+{
+    public static class Day6
+    {
+        private static string day = MethodBase.GetCurrentMethod().DeclaringType.Name;
+
+        public static void CalculateA()
+        {
+            var input = IO.ReadInputFileString(day, "a");
+            int n = 4;
+            int i = 0;
+            for (; i < input.Length - n; i++)
+            {
+                if (input.Substring(i, n).Distinct().Count() == n)
+                    break;
+            }
+
+            IO.WriteOutput(day, "a", i + n);
+        }
+        public static void CalculateB()
+        {
+            var input = IO.ReadInputFileString(day, "a");
+            int n = 14;
+            int i = 0;
+            for (; i < input.Length - n; i++)
+            {
+                if (input.Substring(i, n).Distinct().Count() == n)
+                    break;
+            }
+
+            IO.WriteOutput(day, "b", i + n);
+        }
+    }
+}
