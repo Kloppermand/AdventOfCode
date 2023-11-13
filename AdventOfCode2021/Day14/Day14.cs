826f7a25e7de8b8564b42ef9676cc5ecde3e1044
Kristoffer Andersen <ka@siteshop.dk>
2023-11-13 17:20:20 +0100

    2021 Day 14 B

--- AdventOfCode2021/Day14/Day14.cs
+++ AdventOfCode2021/Day14/Day14.cs
@@ -26,10 +26,64 @@ namespace AdventOfCode2021.Day14
         public static void CalculateB()
         {
             var input = IO.ReadInputFileStringArray(day, "a");
+            string seed = input[0];
 
+            // Make map
+            var map = new Dictionary<string, string>();
+            foreach (var pair in input[2..])
+            {
+                map.Add(pair[0..2], pair[6].ToString());
+            }
 
+            // Populate pairs
+            var pairs = new Dictionary<string, long>();
+            foreach (var pair in map)
+            {
+                pairs.Add(pair.Key, 0);
+            }
 
-            string result = "NOT SOLVED YET";
+            // Set pairs values from input seed
+            for (var i = 1;i < seed.Length; i++)
+            {
+                pairs[seed.Substring(i - 1, 2)]++;
+            }
+
+            // Run insertion
+            for (var i = 0;i < 40; i++) 
+            {
+                // Populate tmpPairs
+                var tmpPairs = new Dictionary<string, long>();
+                foreach (var pair in map)
+                {
+                    tmpPairs.Add(pair.Key, 0);
+                }
+
+                foreach (var pair in pairs)
+                {
+                    tmpPairs[pair.Key.Substring(0, 1) + map[pair.Key]] += pair.Value;
+                    tmpPairs[map[pair.Key] + pair.Key.Substring(1, 1)] += pair.Value;
+                }
+
+                pairs = tmpPairs.ToDictionary(x => x.Key, x => x.Value);
+            }
+
+            // Get element frequency
+            var freq = new Dictionary<char, long>();
+            foreach(var pair in pairs)
+            {
+                freq.TryAdd(pair.Key[0], 0);
+                freq.TryAdd(pair.Key[1], 0);
+
+                freq[pair.Key[0]] += pair.Value;
+                freq[pair.Key[1]] += pair.Value;
+            }
+            // Adjust for start/end element, and half due to double counting
+            freq[seed[0]]++;
+            freq[seed[^1]]++;
+            foreach (var c in freq)
+                freq[c.Key] /= 2;
+
+            string result = (freq.Max(x => x.Value) - freq.Min(x => x.Value)).ToString();
             IO.WriteOutput(day, "b", result);
         }
     }
