8fb960730b84693c3a4d56e4c4b3676a304cd828
Kristoffer Andersen <ka@siteshop.dk>
2023-11-13 12:42:01 +0100

    2019, Day1

---
+++ AdventOfCode2019/Day1/Day1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Utilities;
+
+namespace AdventOfCode2019.Day1
+{
+    public static class Day1
+    {
+        private static string day = MethodBase.GetCurrentMethod().DeclaringType.Name;
+
+        public static void CalculateA()
+        {
+            var input = IO.ReadInputFileIntArray(day, "a");
+
+            int result = input.Sum(x => x / 3 - 2);
+
+            IO.WriteOutput(day, "a", result);
+        }
+        public static void CalculateB()
+        {
+            var input = IO.ReadInputFileIntArray(day, "a");
+
+            int sum = 0;
+            foreach (var x in input)
+            {
+                int tmp = x;
+                while (tmp > 0)
+                {
+                    tmp = Math.Max(tmp / 3 - 2,0);
+                    sum += tmp;
+                }
+            }
+
+            IO.WriteOutput(day, "b", sum);
+        }
+    }
+}
