1dc76ee5d7e76e7de403bfa2be76b7ef83958420
Kristoffer Andersen <ka@siteshop.dk>
2023-12-04 07:54:53 +0100

    2023 Day 1

---
+++ AdventOfCode2023/Day1/Day1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Utilities;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023.Day1
+{
+    public static class Day1
+    {
+        private static string day = MethodBase.GetCurrentMethod().DeclaringType.Name;
+
+        public static void CalculateA()
+        {
+            var input = IO.ReadInputFileStringArray(day, "a");
+
+            var result = input.Sum(s => int.Parse(s.First(c => c >= '0' && c <= '9').ToString() + s.Last(c => c >= '0' && c <= '9').ToString()));
+
+            IO.WriteOutput(day, "a", result);
+        }
+        public static void CalculateB()
+        {
+            var input = IO.ReadInputFileStringArray(day, "a");
+
+            var result = input.Select(s => new Regex(@"(?=(\d|one|two|three|four|five|six|seven|eight|nine))").Matches(s)).Sum(m => int.Parse((m.First().Groups[1].Value + m.Last().Groups[1].Value).Replace("one", "1").Replace("two", "2").Replace("three", "3").Replace("four", "4").Replace("five", "5").Replace("six", "6").Replace("seven", "7").Replace("eight", "8").Replace("nine", "9")));
+            //var oldResult = input.Sum(s => int.Parse(s.Replace("one", "one1one").Replace("two", "two2two").Replace("three", "three3three").Replace("four", "four4four").Replace("five", "five5five").Replace("six", "six6six").Replace("seven", "seven7seven").Replace("eight", "eight8eight").Replace("nine", "nine9nine").First(c => c >= '0' && c <= '9').ToString() + s.Replace("one", "one1one").Replace("two", "two2two").Replace("three", "three3three").Replace("four", "four4four").Replace("five", "five5five").Replace("six", "six6six").Replace("seven", "seven7seven").Replace("eight", "eight8eight").Replace("nine", "nine9nine").Last(c => c >= '0' && c <= '9').ToString()));
+
+            IO.WriteOutput(day, "b", result);
+        }
+    }
+}
---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace AdventOfCode2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1.Day1.CalculateA();
+            Day1.Day1.CalculateB();
+
+            //Day2.Day2.CalculateA();
+            //Day2.Day2.CalculateB();
+
+            //Day3.Day3.CalculateA();
+            //Day3.Day3.CalculateB();
+
+            //Day4.Day4.CalculateA();
+            //Day4.Day4.CalculateB();
+
+            //Day5.Day5.CalculateA();
+            //Day5.Day5.CalculateB();
+
+            //Day6.Day6.CalculateA();
+            //Day6.Day6.CalculateB();
+
+            //Day7.Day7.CalculateA();
+            //Day7.Day7.CalculateB();
+
+            //Day8.Day8.CalculateA();
+            //Day8.Day8.CalculateB();
+
+            //Day9.Day9.CalculateA();
+            //Day9.Day9.CalculateB();
+
+            //Day10.Day10.CalculateA();
+            //Day10.Day10.CalculateB();
+
+            //Day11.Day11.CalculateA();
+            //Day11.Day11.CalculateB();
+
+            //Day12.Day12.CalculateA();
+            //Day12.Day12.CalculateB();
+
+            //Day13.Day13.CalculateA();
+            //Day13.Day13.CalculateB();
+
+            //Day14.Day14.CalculateA();
+            //Day14.Day14.CalculateB();
+
+            //Day15.Day15.CalculateA();
+            //Day15.Day15.CalculateB();
+        }
+    }
+}
