7bf1dc515cd73e83a9b92f62df5ca8a11857456f
Kristoffer Andersen <ka@siteshop.dk>
2024-12-02 14:22:12 +0100

    2024 Day 2

---
+++ AdventOfCode2024/Day2/Day2.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using Utilities;
+
+namespace AdventOfCode2024.Day2
+{
+    public static class Day2
+    {
+        private static string day = MethodBase.GetCurrentMethod().DeclaringType.Name;
+
+        public static void CalculateA()
+        {
+            var input = IO.ReadInputFileStringArray(day, "a");
+            int result = 0;
+
+            foreach (var row in input)
+            {
+                var nums = row.Split(" ").Select(x => int.Parse(x));
+                if (IsReportSafe(nums))
+                    result++;
+            }
+
+
+            IO.WriteOutput(day, "a", result);
+        }
+
+        public static void CalculateB()
+        {
+            var input = IO.ReadInputFileStringArray(day, "a");
+            int result = 0;
+
+            foreach (var row in input)
+            {
+                var nums = row.Split(" ").Select(x => int.Parse(x));
+                var dir = nums.First() > nums.ElementAt(1);
+                var valid = false;
+
+                for (int i = 0; i < nums.Count(); i++)
+                {
+                    var tmpNums = nums.Take(i);
+                    foreach (var tmpNum in nums.TakeLast(nums.Count() - 1 - i))
+                    {
+                        tmpNums = tmpNums.Append(tmpNum);
+                    }
+
+                    if (IsReportSafe(tmpNums))
+                        valid = true;
+                }
+
+                if (valid)
+                    result++;
+            }
+
+            IO.WriteOutput(day, "b", result);
+        }
+
+        private static bool IsReportSafe(IEnumerable<int> nums)
+        {
+            var dir = nums.First() > nums.ElementAt(1);
+            var valid = true;
+
+            for (int i = 0; i < nums.Count() - 1; i++)
+            {
+                if (nums.ElementAt(i) == nums.ElementAt(i + 1)) // No change
+                {
+                    valid = false;
+                    continue;
+                }
+
+                if (Math.Abs(nums.ElementAt(i) - nums.ElementAt(i + 1)) > 3) // Change more than 3
+                {
+                    valid = false;
+                    continue;
+                }
+
+                if (nums.ElementAt(i) > nums.ElementAt(i + 1) != dir) // Changed direction
+                {
+                    valid = false;
+                    continue;
+                }
+            }
+
+            return valid;
+        }
+    }
+}
---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string namespaceName = typeof(Program).Namespace;
+            Console.WriteLine($"===== Running {namespaceName} =====\n");
+
+            RunAll();
+
+            Console.WriteLine($"===== Done running {namespaceName} =====");
+            Console.ReadLine();
+        }
+
+        static void RunAll()
+        {
+            Stopwatch sw = new Stopwatch();
+            string namespaceName = typeof(Program).Namespace;
+            var classes = System.Reflection.Assembly.GetExecutingAssembly().GetTypes().Where(x => x.Namespace.StartsWith(namespaceName) && Regex.IsMatch(x.Name, @"Day(\d{1}|\d{2})"));
+
+            foreach (var day in classes)
+            {
+                var parts = day.GetMethods().Where(x => Regex.IsMatch(x.Name, @"Calculate(A|B)"));
+                foreach (var part in parts)
+                {
+                    sw.Restart();
+                    part.Invoke(null, null);
+                    sw.Stop();
+                    Console.WriteLine($"{namespaceName} {day.Name} {part.Name} ran in approx. {sw.ElapsedMilliseconds / 1000} s");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
