4a8631ae3c628700088e5f72f0e8e6c90d58b13c
Kristoffer Andersen <ka@siteshop.dk>
2024-12-06 12:17:35 +0100

    2024 Day 6

---
+++ AdventOfCode2024/Day6/Day6.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Utilities;
+
+namespace AdventOfCode2024.Day6
+{
+    public static class Day6
+    {
+        private static string day = MethodBase.GetCurrentMethod().DeclaringType.Name;
+
+        public static void CalculateA()
+        {
+            var input = IO.ReadInputFileStringArray(day, "a");
+            var dir = (-1, 0);
+            Dictionary<(int, int), (int, int)> dirChange = new() { { (-1, 0), (0, 1) }, { (0, 1), (1, 0) }, { (1, 0), (0, -1) }, { (0, -1), (-1, 0) } };
+            (int, int) pos = (0, 0);
+            HashSet<(int, int)> visited = new();
+
+            int h = input.Length;
+            int w = input[0].Length;
+
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    if (input[i][j] == '^')
+                        pos = (i, j);
+                }
+            }
+
+            while (true)
+            {
+                visited.Add(pos);
+                var look = (pos.Item1 + dir.Item1, pos.Item2 + dir.Item2);
+                if (look.Item1 < 0 || look.Item1 >= h || look.Item2 < 0 || look.Item2 >= w)
+                    break;
+
+                if (input[look.Item1][look.Item2] != '#')
+                    pos = look;
+                else
+                    dir = dirChange[dir];
+            }
+
+            IO.WriteOutput(day, "a", visited.Count);
+        }
+        public static void CalculateB()
+        {
+            var input = IO.ReadInputFileStringArray(day, "a");
+            var dir = (-1, 0);
+            Dictionary<(int, int), (int, int)> dirChange = new() { { (-1, 0), (0, 1) }, { (0, 1), (1, 0) }, { (1, 0), (0, -1) }, { (0, -1), (-1, 0) } };
+            (int, int) originalPos = (0, 0);
+            int loops = 0;
+
+            int h = input.Length;
+            int w = input[0].Length;
+
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    if (input[i][j] == '^')
+                        originalPos = (i, j);
+                }
+            }
+
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    HashSet<((int, int), (int, int))> visited = new();
+                    (int, int) pos = originalPos;
+                    dir = (-1, 0);
+                    while (true)
+                    {
+                        if (visited.Contains((pos, dir)))
+                        {
+                            loops++;
+                            break;
+                        }
+                        visited.Add((pos, dir));
+                        var look = (pos.Item1 + dir.Item1, pos.Item2 + dir.Item2);
+                        if (look.Item1 < 0 || look.Item1 >= h || look.Item2 < 0 || look.Item2 >= w)
+                            break;
+
+                        if (input[look.Item1][look.Item2] == '#' || look == (i,j))
+                            dir = dirChange[dir];
+                        else
+                            pos = look;
+                    }
+
+                }
+            }
+
+
+            IO.WriteOutput(day, "b", loops);
+        }
+    }
+}
