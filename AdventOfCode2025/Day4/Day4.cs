9214e5a96da31e964a92ae382d15a55d975b3e8a
Kristoffer Andersen <ka@siteshop.dk>
2025-12-05 10:14:55 +0100

    2025 Day4

---
+++ AdventOfCode2025/Day4/Day4.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Utilities;
+
+namespace AdventOfCode2025.Day4
+{
+    public static class Day4
+    {
+        private static string day = MethodBase.GetCurrentMethod().DeclaringType.Name;
+
+        public static void CalculateA()
+        {
+            var input = IO.ReadInputFileStringArray(day, "a");
+            int result = 0;
+
+            for (int y = 0; y < input.Length; y++)
+            {
+                for (int x = 0; x < input[y].Length; x++)
+                {
+                    if (input[y][x] == '@' && Neighbours.CountNeighbours8(x, y, input, '@') < 4)
+                        result++;
+                }
+            }
+
+            IO.WriteOutput(day, "a", result);
+        }
+        public static void CalculateB()
+        {
+            var input = IO.ReadInputFileStringArray(day, "a");
+
+            int result = 0;
+            bool changed = true;
+            while (changed)
+            {
+            var nextIter = new string[input.Length];
+                changed = false;
+                for (int y = 0; y < input.Length; y++)
+                {
+                    for (int x = 0; x < input[y].Length; x++)
+                    {
+                        if (input[y][x] == '@' && Neighbours.CountNeighbours8(x, y, input, '@') < 4)
+                        {
+                            changed = true;
+                            result++;
+                            nextIter[y] += '.';
+                        }
+                        else
+                            nextIter[y] += input[y][x];
+                    }
+                }
+                nextIter.CopyTo(input, 0);
+            }
+
+            IO.WriteOutput(day, "b", result);
+        }
+    }
+}
---
+++ Utilities/Neighbours.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public static class Neighbours
+    {
+        public static int CountNeighbours8(int x, int y, string[] input, char targetChar)
+        {
+            int retValue = 0;
+
+            // x-1
+            if (x - 1 >= 0)
+            {
+                if (y - 1 >= 0)
+                {
+                    if (input[y - 1][x - 1] == targetChar)
+                        retValue++;
+                }
+
+                if (input[y][x - 1] == targetChar)
+                    retValue++;
+
+                if (y + 1 < input.Length)
+                    if (input[y + 1][x - 1] == targetChar)
+                        retValue++;
+            }
+            
+            // x
+            if (y - 1 >= 0)
+            {
+                if (input[y - 1][x] == targetChar)
+                    retValue++;
+            }
+
+            if (y + 1 < input.Length)
+                if (input[y + 1][x] == targetChar)
+                    retValue++;
+
+            // x+1
+            if (x + 1 < input[y].Length)
+            {
+                if (y - 1 >= 0)
+                {
+                    if (input[y - 1][x + 1] == targetChar)
+                        retValue++;
+                }
+
+                if (input[y][x + 1] == targetChar)
+                    retValue++;
+
+                if (y + 1 < input.Length)
+                    if (input[y + 1][x + 1] == targetChar)
+                        retValue++;
+            }
+
+            return retValue;
+        }
+
+        public static int CountNeighbours4(int x, int y, string[] input, char targetChar)
+        {
+            int retValue = 0;
+
+            // x-1
+            if (x - 1 > 0)
+            {
+                if (input[y][x - 1] == targetChar)
+                    retValue++;
+            }
+
+            // x
+            if (y - 1 > 0)
+            {
+                if (input[y - 1][x] == targetChar)
+                    retValue++;
+            }
+
+            if (y + 1 < input.Length)
+                if (input[y + 1][x] == targetChar)
+                    retValue++;
+
+            // x+1
+            if (x + 1 < input[y].Length)
+            {
+                if (input[y][x + 1] == targetChar)
+                    retValue++;
+            }
+
+            return retValue;
+        }
+    }
+}
